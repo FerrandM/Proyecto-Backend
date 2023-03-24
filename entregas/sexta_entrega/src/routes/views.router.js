a33d9c793f3df4ae97f5c68ec6b732cbf5be9324
Ignacio Ferrand <ferrandignacio@gmail.com>
2023-03-23 22:19:51 -0300

    incomplete

---
+++ entregas/sexta_entrega/src/routes/views.router.js
@@ -0,0 +1,29 @@
+const { Router } = require('express')
+
+const routerView = Router()
+
+const fakeapi = [
+    {name:'producto 0', price:400},
+    {name:'producto 1', price:350},
+    {name:'producto 2', price:300},
+    {name:'producto 3', price:200},
+    {name:'producto 4', price:150}
+]
+
+
+routerView.get('/', (req, res)=>{
+    let user = {
+        name:'ignacio',
+        lastName: 'Ferrand',
+        role:'admin'
+    }
+    res.render('index', {
+        user: user,
+        isAdmin: user.role === 'admin',
+        fakeapi,
+        style: 'index.css'
+    })
+})
+
+
+module.exports = routerView
